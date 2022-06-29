975fa378b39cce6f3f21273c2a2f6ab667bbc4e2
marcocalligaris <marcocally@gmail.com>
2022-06-29 17:38:39 +0200

    Fine esercizio palindroma

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
-// ?1 - Come la chiamo?
-// ?2 - Ho bisogno di parametri? (se si quali?)
-// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
-// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?)
-
-// Palindroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
-
-
 // * Palindroma
 
 const userAnswer = prompt('Inserisci una parola');
 
-function isPalindrome(){
+function isPalindrome(word){
     let result = '';
 
-    for (let i = userAnswer.length - 1; i>= 0; i--) {
-        result += userAnswer[i];
+    for (let i = word.length - 1; i>= 0; i--) {
+        result += word[i];
 
-        if ( userAnswer === result) {
-            console.log('La parola è palindroma')
-        }
-    } console.log ('La parola non è palindroma')
+    }
+    if ( word === result) {
+        console.log('La parola è palindroma')
+    } else console.log ('La parola non è palindroma')
 }
 
-
+isPalindrome(userAnswer)
 
 
